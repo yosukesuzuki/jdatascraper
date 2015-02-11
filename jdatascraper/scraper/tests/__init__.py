382f4fde065cb383425f9ef2ee10c7a0e540398a
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-02-12 06:40:55 +0900

    reenable tests for save ids

--- jdatascraper/scraper/tests/__init__.py
+++ jdatascraper/scraper/tests/__init__.py
@@ -3,8 +3,8 @@
 from kay.ext.testutils.gae_test_base import GAETestBase
 from scraper.scrap import get_game_id, scrap_game_data
 from scraper.game_data import test_game_data
-# from scraper.views import save_game_id
-# from core.models import GameId
+from scraper.views import save_game_id
+from core.models import GameId
 
 
 class GetGameIDTest(GAETestBase):
@@ -14,21 +14,12 @@ class GetGameIDTest(GAETestBase):
     def test_get_game_id(self):
         game_ids = get_game_id(2014)
         self.assertEquals(game_ids[0], '15671')
-
-
-'''
-class SaveGameIDTest(GAETestBase):
-    CLEANUP_USED_KIND = True
-    USE_PRODUCTION_STUBS = True
-
-    def test_save_game_id(self):
         game_ids = get_game_id(2014)
         save_game_id(game_ids)
         game_id_object = GameId.get_by_key_name('15671')
         self.assertEquals(game_id_object.game_id, '15671')
         game_count = GameId.all().count(limit=5000)
         self.assertEquals(game_count, 768)
-'''
 
 
 class ScrapGameDataTest(GAETestBase):
